b9c21143d30a0ecfe699808c5d70480559b2dfe0
opensunil <opensunil@gmail.com>
2019-08-13 12:36:24 -0400

    Update sample1.sh

--- sample1.sh
+++ sample1.sh
@@ -1,3 +1,5 @@
+#git clone https://github.com/opensunil/camel-k-demo.git && sh ./camel-k-demo/sample1.sh
+
 wget https://github.com/apache/camel-k/releases/download/1.0.0-M1/camel-k-client-1.0.0-M1-linux-64bit.tar.gz
 gunzip camel-k-client-1.0.0-M1-linux-64bit.tar.gz
 tar -xvf camel-k-client-1.0.0-M1-linux-64bit.tar
