41ddef0f38c7b287780748f91989657e319b14e6
opensunil <opensunil@gmail.com>
2019-08-13 12:35:08 -0400

    Update sample1.sh

--- sample1.sh
+++ sample1.sh
@@ -1,5 +1,5 @@
 wget https://github.com/apache/camel-k/releases/download/1.0.0-M1/camel-k-client-1.0.0-M1-linux-64bit.tar.gz
 gunzip camel-k-client-1.0.0-M1-linux-64bit.tar.gz
 tar -xvf camel-k-client-1.0.0-M1-linux-64bit.tar
-minikube start --wait=false
+minikube start
 ./kamel install --cluster-setup
